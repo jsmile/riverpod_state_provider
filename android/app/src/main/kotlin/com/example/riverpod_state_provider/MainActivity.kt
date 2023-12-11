670996f1f158543f234829fc35ecc80d59a4d8d1
jsmile <jsmile@naver.com>
2023-12-11 17:24:18 +0900

    riverpod StateProvider starter commit

---
+++ android/app/src/main/kotlin/com/example/riverpod_state_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpod_state_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'pages/auto_dispose/auto_dispose_page.dart';
+import 'pages/auto_dispose_family/auto_dispose_family_page.dart';
+import 'pages/basic/basic_page.dart';
+import 'pages/family/family_page.dart';
+import 'widgets/custom_button.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StateProvider',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StateProvider'),
+      ),
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20),
+          children: const [
+            CustomButton(
+              title: 'StateProvider',
+              child: BasicPage(),
+            ),
+            CustomButton(
+              title: 'AutoDisposeStateProvider',
+              child: AutoDisposePage(),
+            ),
+            CustomButton(
+              title: 'FamilyStateProvider',
+              child: FamilyPage(),
+            ),
+            CustomButton(
+              title: 'AutoDisposeFamilyStateProvider',
+              child: AutoDisposeFamilyPage(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
